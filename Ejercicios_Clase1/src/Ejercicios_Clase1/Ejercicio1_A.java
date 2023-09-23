2d1b3d92c2acac7d924d803fe507de0dba173c85
JuanB1986 <mj.b86@hotmail.com>
2023-09-23 19:05:44 -0300

    ejercicios

---
+++ Ejercicios_Clase1/src/Ejercicios_Clase1/Ejercicio1_A.java
@@ -0,0 +1,20 @@
+package Ejercicios_Clase1;
+
+public class Ejercicio1_A {
+
+	public static void main(String[] args) {
+		int numInicio=5;
+		int numFin=14;
+		int contador=0;
+		
+		contador=numInicio;
+		
+		while ( contador <= numFin) 
+		{
+			System.out.println(contador);			
+			++contador;
+		}
+		
+	}
+
+}
