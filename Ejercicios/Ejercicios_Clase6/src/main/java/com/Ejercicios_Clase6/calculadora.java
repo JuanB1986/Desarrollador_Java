fb112d47edb927704024132c6f1aedb09ba4a882
JuanB1986 <mj.b86@hotmail.com>
2023-10-11 19:38:19 -0300

    Anexo Ejercicio 6

---
+++ Ejercicios/Ejercicios_Clase6/src/main/java/com/Ejercicios_Clase6/calculadora.java
@@ -0,0 +1,22 @@
+package com.Ejercicios_Clase6;
+
+public class calculadora {
+	
+	
+	public static double sumar(double unNumero, double otroNumero) {
+		return unNumero+otroNumero;
+	}
+	
+	public static double restar(double unNumero, double otroNumero) {
+		return unNumero-otroNumero;
+	}
+	
+	public static double dividir(double unNumero, double otroNumero) {
+		return unNumero/otroNumero;
+	}
+	
+	public static double multiplicar(double unNumero, double otroNumero) {
+		return unNumero*otroNumero;
+	}
+	
+}
