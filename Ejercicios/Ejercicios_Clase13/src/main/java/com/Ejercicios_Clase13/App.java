3aeb7415e2ef58391263d3ffe63af5886c393da0
JuanB1986 <mj.b86@hotmail.com>
2023-11-08 17:03:06 -0300

    Ejercicio 13 - Rev1

--- Ejercicios/Ejercicios_Clase13/src/main/java/com/Ejercicios_Clase13/App.java
+++ Ejercicios/Ejercicios_Clase13/src/main/java/com/Ejercicios_Clase13/App.java
@@ -1,81 +1,38 @@
 package com.Ejercicios_Clase13;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
-import java.sql.SQLException;
-import java.sql.Statement;
-
-
-
-import java.sql.SQLData;
-
 public class App 
 {
-	public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	public static final String DB_URL = "jdbc:mysql://localhost:3306/qatar2022";
-	
+	//public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	public static final String DB_URL = "jdbc:mysql://localhost:3306/qatar2022";	
 	public static final String user = "root";
 	public static final String password = "juan";
 	
     public static void main( String[] args )
     {	
     	
-    	Connection connection=null;
-    	Statement statement=null;
-    	PreparedStatement pStatement;
-    	    	
-    	//Lee tabla empleado
-    	try {
-    		connection = DriverManager.getConnection(DB_URL, user, password);    		
-    		statement = connection.createStatement();    		
-    		ResultSet querry = statement.executeQuery("SELECT * FROM empleado");    		
-    		String base="";    		
-    		while (querry.next())
-    		{
-    			base += querry.getString("nombre")+" "+querry.getString("apellido")+" "+querry.getString("nacionalidad")+" "+querry.getString("dni")+"\n";    			
-    		}    		
-    		System.out.println(base);
-    		querry.close();    		
-    		connection.close();    		
+    	SQL objSQL=new SQL();
+    	
+    	//Inicio conexion
+    	if (objSQL.SQLConnect(DB_URL, user, password))
+    	{    		   	    	
+	    	//Agrega campos: Si ya existen los campos, ejecuta la excepcion dentro de SQLQuerry()
+	    	objSQL.SQLQuerry("INSERT INTO empleado\nVALUES ('1', 'Thomas', 'Edison', 'Estadounidense', '111222', '12');");  
+	    	objSQL.SQLQuerry("INSERT INTO empleado\nVALUES ('2', 'Nikola', 'Tesla', 'Servio', '1236896', '12');"); 
+	    	objSQL.SQLQuerry("INSERT INTO empleado\nVALUES ('4', 'Enrique', 'Gaviola', 'Argentino', '132435', '11');");    	
+	    	objSQL.SQLQuerry("INSERT INTO empleado\nVALUES ('5', 'Steve', 'Jobs', 'Estadounidense', '321654', '12');");	
+	    	objSQL.SQLQuerry("INSERT INTO empleado\nVALUES ('6', 'Alan', 'Turin', 'Ingles', '147258', '11');");	
+	    	
+	    	//Consulta: desde tabla y campos.
+	    	System.out.println(objSQL.SQLRead("empleado", "apellido,nombre,nacionalidad,dni,idEmpleado"));
+	    	
+	    	//Actualiza campo nombres.
+	    	objSQL.SQLQuerry("UPDATE empleado SET nombre='Alan Mathison' WHERE nombre = 'Alan'");
+	    	
+	    	//Borra filas de la base de datos.
+	    	objSQL.SQLQuerry("DELETE FROM empleado WHERE nombre = 'Thomas'");
+	    	
+	    	//Consulta: desde tabla y campos.
+	    	System.out.println(objSQL.SQLRead("empleado", "apellido,nombre,nacionalidad,dni,idEmpleado"));
     	}
-    	catch(SQLException e) {System.out.println( "SELECT FROM: Failed" );}    
-    	
-    	
-    	//Agrega campos a la tabla empleado
-    	try {
-    		String campo = "INSERT INTO empleado\nVALUES ('3', 'Albert', 'Einstein', 'Aleman', '132435', '11');";
-    		connection = DriverManager.getConnection(DB_URL, user, password);    		
-    		pStatement = connection.prepareStatement(campo);    		
-    		pStatement.executeUpdate();    		
-    		pStatement.close();    		
-    		connection.close();
-    	}
-    	catch(SQLException e) {System.out.println( "INSERT: Failed" );} 
-    	
-    	
-    	//Actualiza nombre empleado
-    	try {
-    		String campo = "UPDATE empleado SET nombre = 'Juan Manuel' WHERE nombre = 'Juan'";
-    		connection = DriverManager.getConnection(DB_URL, user, password);    		
-    		pStatement = connection.prepareStatement(campo);    		
-    		pStatement.executeUpdate();    		
-    		pStatement.close();    		
-    		connection.close();
-    	}
-    	catch(SQLException e) {System.out.println( "UPDATE: Failed" );} 
-    	    	
-    	
-    	//Borra campo de la tabla de empleado
-    	try {
-    		connection = DriverManager.getConnection(DB_URL, user, password);    		
-    		pStatement = connection.prepareStatement("DELETE FROM empleado WHERE nombre='Juan Manuel'");    		
-    		pStatement.executeUpdate();    		
-    		pStatement.close();    		
-    		connection.close();
-    	}
-    	catch(SQLException e) {System.out.println( "DELETE: Failed" );}     	
     }
 }
---
+++ Ejercicios/Ejercicios_Clase13/src/main/java/com/Ejercicios_Clase13/SQL.java
@@ -0,0 +1,69 @@
+package com.Ejercicios_Clase13;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQL {
+	
+	private Connection connection;
+	private PreparedStatement preparedStatement;
+	private Statement statement;
+	private ResultSet resultSet;
+	
+	public SQL() {}
+	
+	//Conexion a base de datos
+	public boolean  SQLConnect(String url, String user, String password) {
+		try {
+			connection = DriverManager.getConnection(url, user, password);
+			return !connection.isClosed();
+		}
+		catch(SQLException e) {return false;}
+	}
+	
+	
+	//Consulta con SELECT * FROM
+	public String SQLRead(String tabla, String columnas) {		
+		String split[] = columnas.split(",");
+		String retVal=""; 		
+	   	try {    			
+    		statement = connection.createStatement();    		
+    		resultSet = statement.executeQuery("SELECT * FROM "+tabla); 
+    		
+    		while (resultSet.next()) {
+    			for (int i=0; i<split.length;++i) {
+    				retVal += resultSet.getString(split[i].strip())+", ";
+    			}
+    			retVal += "\n";
+    		}
+    		return retVal;
+    	}
+    	catch(SQLException e) {return "SQLRead_Exception";} 		
+	}
+	
+	
+	//Consultas: INSERT, UPDATE
+	public boolean SQLQuerry(String querry) {
+		try {			
+ 			preparedStatement = connection.prepareStatement(querry);    		
+			preparedStatement.executeUpdate();   
+			return true;
+		}
+		catch(SQLException e) {return false;} 
+	}
+		
+	//Cierra conexión
+	public boolean SQLClose() {
+		try {
+			statement.close();
+			preparedStatement.close();
+			connection.close();
+			return connection.isClosed();
+		}
+		catch(SQLException e) {return false;}
+	}
+}
